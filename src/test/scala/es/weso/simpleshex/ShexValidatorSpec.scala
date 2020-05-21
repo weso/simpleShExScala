af0de3b46e93e9e8f2e6dc6bb99f1d278bfc5819
Toni Cebrián <toni.cebrian@dowjones.com>
2020-05-21 16:46:32 +0200

    Make tests intention more clear

--- src/test/scala/es/weso/simpleshex/ShexValidatorSpec.scala
+++ src/test/scala/es/weso/simpleshex/ShexValidatorSpec.scala
@@ -1,13 +1,15 @@
 package es.weso.simpleshex
 
 import es.weso.rdf.rdf4j.RDFAsRDF4jModel
-import es.weso.shapeMaps.ShapeMap
+import es.weso.shapeMaps.{IRILabel, ShapeMap}
 import es.weso.shex.Schema
 import es.weso.shex.validator.Validator
 import org.eclipse.rdf4j.rio.{RDFFormat, Rio}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import Either2IO._
+import es.weso.rdf.nodes
+import es.weso.rdf.nodes.{IRI, RDFNode}
 
 import scala.io.Source
 import es.weso.shex.ResolvedSchema
@@ -23,25 +25,30 @@ class ShexValidatorSpec extends AnyFlatSpec with Matchers {
 
     val validation = for {
       shex <- Schema.fromString(shexDefinition.getLines.mkString, "ShexC")
-      shapeMap <- either2IO(ShapeMap.fromString(mappingsDeclaration.getLines().mkString,
-                                      ShapeMap.defaultFormat,
-                                      None,
-                                      rdf.getPrefixMap(),
-                                      shex.prefixMap))
-      fixedShapeMap <- ShapeMap.fixShapeMap(shapeMap, rdf, rdf.getPrefixMap, shex.prefixMap)
-      resolvedSchema <- ResolvedSchema.resolve(shex,None)
-      result        <- Validator.validate(resolvedSchema, fixedShapeMap, rdf)
-      resultMap <- result.toResultShapeMap
+      shapeMap <- either2IO(
+        ShapeMap.fromString(mappingsDeclaration.getLines().mkString,
+                            ShapeMap.defaultFormat,
+                            None,
+                            rdf.getPrefixMap(),
+                            shex.prefixMap))
+      fixedShapeMap  <- ShapeMap.fixShapeMap(shapeMap, rdf, rdf.getPrefixMap, shex.prefixMap)
+      resolvedSchema <- ResolvedSchema.resolve(shex, None)
+      result         <- Validator.validate(resolvedSchema, fixedShapeMap, rdf)
+      resultMap      <- result.toResultShapeMap
     } yield resultMap
 
     validation.attempt.unsafeRunSync match {
       case Right(result) =>
-        result.noSolutions should be(false)
+        val userShape         = IRILabel(IRI("http://example.org/User"))
+        val alice             = IRI("http://example.org/alice")
+        val bob               = IRI("http://example.org/bob")
+        val conformantsAlice  = result.getConformantShapes(alice)
+        val nonConformantsBob = result.getNonConformantShapes(bob)
+        conformantsAlice should contain allElementsOf List(userShape)
+        nonConformantsBob should contain allElementsOf List(userShape)
 
       case Left(e) =>
         fail(e)
     }
   }
-
-
 }
